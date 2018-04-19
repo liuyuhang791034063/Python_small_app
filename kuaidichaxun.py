8b3d7a939107edce4461e7324154223d472863cd
liuyuhang791034063 <liuyuhang791034063@qq.com>
2018-04-19 19:30:25 +0800

    快递查询脚本

--- kuaidichaxun.py
+++ kuaidichaxun.py
@@ -6,5 +6,3 @@ compangName = json.loads(requests.get(url1).text)['auto'][0]['comCode']
 url2 = 'http://www.kuaidi100.com/query?type=' + compangName + '&postid=' + packageNum
 for item in json.loads(requests.get(url2).text)['data']:
     print(item['time'],item['context'])
-top = Tkinter.Tk()
-top.mainloop()
