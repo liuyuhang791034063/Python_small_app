1a28b145398c33dda5bf5619448163a7124dd6f3
liuyuhang791034063 <liuyuhang791034063@qq.com>
2019-12-18 12:59:45 +0800

    修改wallhaven壁纸爬虫的逻辑

--- wallhaven_spdier/wallhaven_spdier/spiders/spdier.py
+++ wallhaven_spdier/wallhaven_spdier/spiders/spdier.py
@@ -21,15 +21,16 @@ from ..config import PAGE_NUMBER, BAN_TAGS
 
 class Spider(CrawlSpider):
     name = 'wallhaven'
-    allowed_domains = ['alpha.wallhaven.cc']
-    bash_url = 'https://alpha.wallhaven.cc/random?page='
+    allowed_domains = ['wallhaven.cc']
+    bash_url = 'https://wallhaven.cc/latest?page='
 
     def start_requests(self):
         for i in range(1, PAGE_NUMBER + 1):
             yield Request(self.bash_url + str(i))
 
     rules = (
-        Rule(LinkExtractor(allow=('https://alpha.wallhaven.cc/wallpaper/\d{1,6}$',)), callback='parse_item'),
+        # Rule(LinkExtractor(allow=('https://wallhaven.cc/wallpaper/\d{1,6}$',)), callback='parse_item'),
+        Rule(LinkExtractor(allow=('https://wallhaven.cc/w/\w{1,6}$',)), callback='parse_item'),
     )
 
     def parse_item(self, response):
@@ -39,7 +40,7 @@ class Spider(CrawlSpider):
         for tag in ban_tags:
             if tag in tags:
                 return
-        item['url'] = str('https:' + response.xpath('//*[@id="wallpaper"]/@src').extract_first(default=0))
+        item['url'] = str(response.xpath('//*[@id="wallpaper"]/@src').extract_first(default=0))
         item['name'] = item['url'].split('/')[-1].split('.')[0]
         item['type'] = item['url'].split('/')[-1].split('.')[1]
         yield item
